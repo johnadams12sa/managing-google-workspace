bc1a07a8121b851a26f493c84e24c886c0ebaabb
Aaron Yam <ayam5005@gmail.com>
2021-06-27 14:36:13 -0400

    Commented out all OAuth lines
    Only using service account moving forward to avoid the use of OAuth authentication, as admins of the org, we should be changing this without needing the permission of end user to create standard signature

--- changeSignature.py
+++ changeSignature.py
@@ -10,35 +10,36 @@ from google.oauth2 import service_account
 
 # VARIABLE DECLARATIONS / BUILDING OBJECTS
 API_scopes = ['https://www.googleapis.com/auth/gmail.settings.basic', 'https://www.googleapis.com/auth/gmail.settings.sharing']
-service_account_file = 'path_to_service_account_key/service_account_key.json'
+service_account_file = '/Users/aaron.yam/Desktop/googleapi/service_account_key.json'
 
 #command line arguments set into local variables
 cli_input_first_name = sys.argv[1]
 cli_input_last_name = sys.argv[2]
 cli_input_job_title = sys.argv[3]
 cli_input_email = sys.argv[4]
-
+cli_input_pronouns = sys.argv[5]
+cli_input_phone_number = sys.argv[6]
 
 def main():
 	#checks for access token to use Gmail API, if not, requests one, and prompts users to authorize app with given scope
-	creds = None
-	if os.path.exists('token.json'):
-		creds = Credentials.from_authorized_user_file('token.json', API_scopes)
-	if not creds or not creds.valid:
-		if creds and creds.expired and creds.refresh_token:
-			creds.refresh(Request())
-		else:
-			flow = InstalledAppFlow.from_client_secrets_file(
-				'credentials.json', API_scopes)
-			creds = flow.run_local_server(port=0)
-		with open('token.json','w') as token:
-			token.write(creds.to_json())	
+	#creds = None
+	#if os.path.exists('token.json'):
+	#	creds = Credentials.from_authorized_user_file('token.json', API_scopes)
+	#if not creds or not creds.valid:
+	#	if creds and creds.expired and creds.refresh_token:
+	#		creds.refresh(Request())
+	#	else:
+	#		flow = InstalledAppFlow.from_client_secrets_file(
+	#			'credentials.json', API_scopes)
+	#		creds = flow.run_local_server(port=0)
+	#	with open('token.json','w') as token:
+	#		token.write(creds.to_json())	
 
 
 #Creating API Objects / Service Accounts	
 	
 	#creates service object for interacting with Gmail API
-	gmail_service = build('gmail', 'v1', credentials=creds)
+	#gmail_service = build('gmail', 'v1', credentials=creds)
 
 	#creating service_account credentials to use to "impersonate" user in domain, domain wide delegation was granted to this service account
 	credentials = service_account.Credentials.from_service_account_file('/Users/aaron.yam/Desktop/googleapi/service_account_key.json', scopes=API_scopes)
@@ -66,12 +67,15 @@ def main():
 		signature = file.read()	
 
 	#replace the boilerplate email template with parameters fed in from the command line
-	new_signature = signature.replace('first_name_placeholder', cli_input_first_name).replace('last_name_placeholder', cli_input_last_name).replace('Job_Title', cli_input_job_title).replace('email_placeholder',  cli_input_email).replace('email_link_placeholder', 'mailto:' + 'email_placeholder'+ '@brooklynminds.com')
+	#new_signature = signature.replace('first_name_placeholder', cli_input_first_name).replace('last_name_placeholder', cli_input_last_name).replace('Job_Title', cli_input_job_title).replace('email_placeholder',  cli_input_email).replace('email_link_placeholder', 'mailto:' + 'email_placeholder'+ '@brooklynminds.com')
+
+	new_signature = signature.replace('first_name_placeholder', cli_input_first_name).replace('last_name_placeholder', cli_input_last_name).replace('Job_Title', cli_input_job_title).replace('pronouns', cli_input_pronouns).replace('phone_number_placeholder', cli_input_phone_number)
 
 	#signature.replace('last_name_placeholder', cli_input_last_name)
 	#signature.replace('email_placeholder', cli_input_email)	
 	#signature.replace('email_link_placeholder', "mailto:" + "email_placeholder" + "@brooklynminds.com")
 
+	#print(new_signature)
 	#stores the signature into the configuration to be pushed to the account
 	sendAsConfiguration = {
 		'signature' : new_signature 
