c39cd1a9c0a8369e827b398968c67089c8cb7ae3
Aaron Yam <ayam5005@gmail.com>
2021-07-20 23:23:51 -0400

    marked script with comments

--- listDelegate.py
+++ listDelegate.py
@@ -6,14 +6,17 @@ import os.path
 service_account_file = '/Users/aaron.yam/Desktop/googleapi/service_account_key.json'
 API_scopes = ['https://www.googleapis.com/auth/gmail.settings.basic', 'https://www.googleapis.com/auth/gmail.settings.sharing']
 
+#command line argument only requires user of same domain
 cli_input_email = sys.argv[1]
-#cli_input_delegateEmail = sys.argv[2]
 
 def main():
+	#creates credentials for service account using the email address within the same organization domain and creates a 
+	#service object to interact with the Gmail API
 	credentials = service_account.Credentials.from_service_account_file(service_account_file, scopes=API_scopes)
 	delegated_credentials = credentials.with_subject(cli_input_email)
 	gmail_service = build('gmail','v1',credentials=delegated_credentials)
 
+	#returns a list of delegates currently in user's account
 	results = gmail_service.users().settings().delegates().list(userId='me').execute()
 	print(results)
 
